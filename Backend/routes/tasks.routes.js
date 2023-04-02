94d0c7bfc7db7f45f539787653a97f43258243ac
JRaghu842 <jraghavendra842@gmail.com>
2023-04-02 16:53:52 +0530

    All functionality done just logout part remaining

--- Backend/routes/tasks.routes.js
+++ Backend/routes/tasks.routes.js
@@ -14,10 +14,76 @@ taskRouter.post("/add", async (req, res) => {
   }
 });
 
+// taskRouter.get("/", async (req, res) => {
+//   let token = req.headers.authorization;
+//   let decoded = jwt.verify(token, "monday");
+//   let {
+//     board,
+//     task,
+//     status,
+//     person_allocated,
+//     p_email,
+//     start_date,
+//     end_date,
+//     user,
+//     sortby,
+//     page,
+//     limit,
+//   } = req.query;
+//   try {
+//     if (decoded) {
+//       let query = {};
+//       if (board) {
+//         query.board = { $regex: board, $options: "i" };
+//       }
+//       if (task) {
+//         query.task = { $regex: task, $options: "i" };
+//       }
+//       if (status) {
+//         query.status = { $regex: status, $options: "i" };
+//       }
+//       if (person_allocated) {
+//         query.person_allocated = { $regex: person_allocated, $options: "i" };
+//       }
+
+//       if (p_email) {
+//         query.p_email = { $regex: p_email, $options: "i" };
+//       }
+//       if (start_date) {
+//         query.start_date = { $gte: new Date(start_date) };
+//       }
+//       if (end_date) {
+//         query.end_date = { $lte: new Date(end_date) };
+//       }
+
+//       query.user = decoded.userID;
+
+//       let sort = {};
+
+//       if (sortby) {
+//         sort[sortby] = 1;
+//       }
+
+//       let pagenumber = Number(page) || 1;
+//       let pagelimit = Number(limit) || 10;
+//       let skip = (pagenumber - 1) * pagelimit;
+
+//       let tasks = await TaskModel.find(query)
+//         .sort(sort)
+//         .skip(skip)
+//         .limit(pagelimit);
+//       res.status(200).send({ msg: `Here are all the tasks `, tasks });
+//     }
+//   } catch (error) {
+//     res.status(400).send({ msg: error.message });
+//   }
+// });
+
 taskRouter.get("/", async (req, res) => {
   let token = req.headers.authorization;
   let decoded = jwt.verify(token, "monday");
   let {
+    board,
     task,
     status,
     person_allocated,
@@ -25,13 +91,25 @@ taskRouter.get("/", async (req, res) => {
     start_date,
     end_date,
     user,
-    sortby,
+    sortbyasc,
+    sortbydesc,
     page,
     limit,
+    search,
+    fields,
   } = req.query;
   try {
     if (decoded) {
       let query = {};
+      if (search) {
+        query["$or"] = fields
+          .split(",")
+          .map((field) => ({ [field]: { $regex: search, $options: "i" } }));
+      }
+      if (board) {
+        query.board = { $regex: board, $options: "i" };
+        // query.board = board;
+      }
       if (task) {
         query.task = { $regex: task, $options: "i" };
       }
@@ -40,6 +118,7 @@ taskRouter.get("/", async (req, res) => {
       }
       if (person_allocated) {
         query.person_allocated = { $regex: person_allocated, $options: "i" };
+        // query.person_allocated = person_allocated;
       }
 
       if (p_email) {
@@ -56,8 +135,12 @@ taskRouter.get("/", async (req, res) => {
 
       let sort = {};
 
-      if (sortby) {
-        sort[sortby] = 1;
+      if (sortbyasc) {
+        sort[sortbyasc] = 1;
+      }
+
+      if (sortbydesc) {
+        sort[sortbydesc] = -1;
       }
 
       let pagenumber = Number(page) || 1;
